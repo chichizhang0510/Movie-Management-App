e319652c56ee0d2a0d74eca9ab5b7082eff196bf
Chichi Zhang <chichizhang20000510@gmail.com>
2025-03-17 04:55:02 -0700

    deleted useless content

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import StarRating from "./components/StarRating";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    {/* <StarRating
-      maxRating={5}
-      messages={["terrible", "bad", "okay", "good", "amazing"]}
-      defaultRating={3}
-    />
-    <StarRating maxRating={10} defaultRating={6} />
-    <StarRating maxRating={4} size={24} defaultRating={2} /> */}
   </StrictMode>
 );
